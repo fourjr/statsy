d8356f8564d025e4978e125b6c564efe9d23d4d1
kwugfighter <isaacli430@gmail.com>
2017-11-06 20:13:17 +0800

    nvm

--- statsbot.py
+++ statsbot.py
@@ -389,7 +389,7 @@ class StatsBot(commands.AutoShardedBot):
 
         await ctx.send(embed=em)
 
-    @commands.group(invoke_without_command=False)
+    @commands.group(invoke_without_command=True)
     async def save(self, ctx):
         '''Saves a gamertag to your discord profile.
 
