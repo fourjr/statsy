dd6579aa5768197b8c5161d63680124716bd2d2b
Jia Rong Yee <yeejiarong2004@gmail.com>
2017-12-04 17:00:16 +0800

    BRAWLSTARS!!

---
+++ cogs/brawlstars.py
@@ -0,0 +1,154 @@
+import discord
+from discord.ext import commands
+from bs4 import BeautifulSoup
+from statsbot import InvalidTag
+from ext import embeds_bs
+from ext.paginator import PaginatorSession
+
+class TagCheck(commands.MemberConverter):
+    
+    check = 'PYLQGRJCUV0289'
+
+    def resolve_tag(self, tag):
+        tag = tag.strip('#').upper().replace('O','0')
+        if any(i not in self.check for i in tag):
+            return False
+        else:
+            return tag
+
+    async def convert(self, ctx, argument):
+        # Try to convert it to a member.
+        try:
+            user = await super().convert(ctx, argument)
+        except commands.BadArgument:
+            pass 
+        else:
+            return user
+
+        # Not a user so its a tag.
+        tag = self.resolve_tag(argument)
+
+        if not tag:
+            raise InvalidTag('Invalid bs-tag passed.')
+        else:
+            return tag
+        
+class BrawlStars:
+
+    '''Commands relating to the Brawl Stars game made by supercell.'''
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.url = 'https://brawlstats.io/'
+        self.conv = TagCheck()
+
+    async def get_band_from_profile(self, ctx, tag, message):
+        url = 'https://brawlstats.io/' + 'players/' + tag
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+            'x-requested-with': 'XMLHttpRequest'
+        }
+        await ctx.session.get(url + '/update', headers=headers)
+        del headers['x-requested-with']
+        async with ctx.session.get(url, headers=headers) as resp:
+            soup = BeautifulSoup(await resp.text(), 'html.parser')
+        try:
+            band_tag = soup.find('main') \
+                .find('section', attrs={'class':'ui-card pt-4'}) \
+                .find('div', attrs={'class':'container'}) \
+                .find('div', attrs={'class':'stat-section'}) \
+                .find_all('div', attrs={'class':'col-12 mt-1'})[2] \
+                .find('div', attrs={'class':'band-history-entry'}) \
+                .find('a') \
+                .find('div', attrs={'class':'card jumpc mb-2'}) \
+                .find('div', attrs={'class':'card-body'}) \
+                .find('div', attrs={'class':'band-info'}) \
+                .find('div', attrs={'class':'band-tag'}).getText().strip('#')
+        except AttributeError:
+            await ctx.send(message)
+            raise ValueError(message)
+        else:
+            return band_tag
+
+    async def resolve_tag(self, ctx, tag_or_user, band=False):
+        if not tag_or_user:
+            try:
+                tag = ctx.get_tag('brawlstars')
+            except Exception as e:
+                print(e)
+                await ctx.send('You don\'t have a saved tag.')
+                raise e
+            else:
+                if band is True:
+                    return await self.get_band_from_profile(ctx, tag, 'You don\'t have a band!')
+                return tag
+        if isinstance(tag_or_user, discord.Member):
+            try:
+                tag = ctx.get_tag('brawlstars', tag_or_user.id)
+            except KeyError as e:
+                await ctx.send('That person doesnt have a saved tag!')
+                raise e
+            else:
+                if band is True:
+                    return await self.get_band_from_profile(ctx, tag, 'That person does not have a band!')
+                return tag
+        else:
+            return tag_or_user
+
+    @commands.command()
+    async def bssave(self, ctx, *, tag):
+        '''Saves a Brawl Stars tag to your discord profile.
+
+        Ability to save multiple tags coming soon.
+        '''
+        tag = self.conv.resolve_tag(tag)
+
+        if not tag:
+            raise InvalidTag('Invalid tag') 
+
+        ctx.save_tag(tag, 'brawlstars')
+
+        await ctx.send('Successfully saved tag.')
+
+
+    @commands.command()
+    async def bsprofile(self, ctx, tag_or_user:TagCheck=None):
+        async with ctx.channel.typing():
+            tag = await self.resolve_tag(ctx, tag_or_user)
+            url = self.url + 'players/' + tag
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+                'x-requested-with': 'XMLHttpRequest'
+            }
+            await ctx.session.get(url + '/update', headers=headers)
+            del headers['x-requested-with']
+            async with ctx.session.get(url, headers=headers) as resp:
+                soup = BeautifulSoup(await resp.text(), 'html.parser')
+
+            em = await embeds_bs.format_profile(ctx, soup, tag)
+            await ctx.send(embed=em)
+
+    @commands.command()
+    async def bsband(self, ctx, tag_or_user:TagCheck=None): 
+        async with ctx.channel.typing():
+            tag = await self.resolve_tag(ctx, tag_or_user, band=True)
+            url = self.url + 'bands/' + tag
+            headers = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0',
+                'x-requested-with': 'XMLHttpRequest'
+            }
+            await ctx.session.get(url + '/refresh', headers=headers)
+            del headers['x-requested-with']
+            async with ctx.session.get(url, headers=headers) as resp:
+                soup = BeautifulSoup(await resp.text(), 'html.parser')
+
+            ems = await embeds_bs.format_band(ctx, soup, tag)
+        session = PaginatorSession(
+            ctx=ctx,
+            pages=ems
+            )
+        await session.run()
+
+def setup(bot):
+    cog = BrawlStars(bot)
+    bot.add_cog(cog)
---
+++ ext/embeds_bs.py
@@ -0,0 +1,183 @@
+import discord
+import random
+import copy
+from statsbot import InvalidTag
+from bs4 import BeautifulSoup
+
+def random_color():
+    return random.randint(0, 0xFFFFFF)
+
+def emoji(ctx, name):
+    name = name.lower().replace('ricochet', 'rico').replace('el primo', 'primo').replace('jessie', 'jess').replace('dynamike', 'mike')
+    e = discord.utils.get(ctx.bot.game_emojis, name=name)
+    if e is None:
+        return name
+    return e
+
+url = 'https://brawlstats.io'
+
+async def format_profile(ctx, soup, tag):
+    try:
+        profile = soup.find('main') \
+                .find('section', attrs={'class':'ui-card pt-4'}) \
+                .find('div', attrs={'class':'container'}) \
+                .find('div', attrs={'class':'stat-section'})
+    except AttributeError:
+        raise InvalidTag('Invalid bs-tag passed.')
+    name = profile.find('div', attrs={'class':'row'}) \
+        .find('div', attrs={'class':'col-12'}) \
+        .find('div', attrs={'class':'player-profile text-center'}) \
+        .find('div', attrs={'class':'player-info'}) \
+        .find('div', {'class':'player-name brawlstars-font'}).getText()
+
+    brawlersraw = profile.find_all('div', attrs={'class':'col-12 mt-1'})[1].find_all('div')
+    brawlers = ''
+    for brawler in brawlersraw:
+        try:
+            brawlers += str(emoji(ctx, \
+            brawler.find('a').find('div').find('div') \
+            .find('div', attrs={'class':'name'}).getText()))
+        except AttributeError:
+            pass
+
+    pic = url + profile.find('div', attrs={'class':'row'}) \
+        .find('div', attrs={'class':'col-12'}) \
+        .find('div', attrs={'class':'player-profile text-center'}) \
+        .find('div', attrs={'class':'player-info'}) \
+        .find('div', {'class':'profile-avatar'}) \
+        .find('img')['src']
+
+    trophies = profile.find('div', attrs={'class':'col-6 col-md-4 col-lg-3 mb-2'}).getText().strip('Trophies')
+    pb = profile.find_all('div', attrs={'class':'col-6 col-md-4 col-lg-3 mb-2'})[1].getText().strip('Highest trophies')
+    victories = profile.find_all('div', attrs={'class':'col-6 col-md-4 col-lg-3 mb-2'})[2].getText().strip('Victories')
+    showdown = profile.find_all('div', attrs={'class':'col-6 col-md-4 col-lg-3 mb-2'})[3].getText().strip('Showdown victories')
+    expvals = profile.find('div', attrs={'class':'row'}) \
+            .find('div', attrs={'class':'col-12'}) \
+            .find('div', attrs={'class':'player-profile text-center'}) \
+            .find('div', attrs={'class':'experience-bar mt-3'})
+    exp = expvals.find('div', attrs={'class':'experience-level'}).getText() + ' (' + \
+         expvals.find('div', attrs={'class':'progress-text'}).getText() + ')'
+    bandtag = profile.find_all('div', attrs={'class':'col-12 mt-1'})[2] \
+                .find('div', attrs={'class':'band-history-entry'}) \
+                .find('a')['href'].strip('/bands/')
+    bandname = profile.find_all('div', attrs={'class':'col-12 mt-1'})[2] \
+                .find('div', attrs={'class':'band-history-entry'}) \
+                .find('a') \
+                .find('div', attrs={'class':'card jumpc mb-2'}) \
+                .find('div', attrs={'class':'band-info'}) \
+                .find('div', attrs={'class':'band-name mr-2'}).getText()
+
+    em = discord.Embed(color=random_color())
+    if ctx.bot.psa_message:
+        em.description = f'*{ctx.bot.psa_message}*'
+    em.set_author(name=f'{name} #{tag}')
+    em.set_thumbnail(url=pic)
+
+    embed_fields = [
+        ('Trophies', f'{trophies}/{pb} PB {emoji(ctx, "icon_trophy")}', True),
+        ('Victories', f'{victories} {emoji(ctx, "star_gold_00")}', True),
+        ('Showdown Wins', f'{showdown} {emoji(ctx, "icon_showdown")}', True),
+        ('Level', f'{exp} {emoji(ctx, "star_silver")}', True),
+        ('Band Name', bandname, True),
+        ('Band Tag', '#' + bandtag, True),
+        ('Brawlers', brawlers, False),
+    ]
+
+    for n, v, i in embed_fields:
+        if v:
+            em.add_field(name=n, value=v, inline=i)
+
+    return em
+
+async def format_band(ctx, soup, tag):
+    try:
+        band = soup.find('main') \
+                .find('section', attrs={'class':'ui-card pt-4'}) \
+                .find('div', attrs={'class':'container'}) \
+                .find('div', attrs={'class':'stat-section'})
+    except AttributeError:
+        raise InvalidTag('Invalid bs-tag passed.')
+    bandinfo = band.find('div', attrs={'class':'row'}) \
+            .find('div', attrs={'class':'col-12'}) \
+            .find('div', attrs={'class':'band-profile text-center'}) \
+            .find('div', attrs={'class':'col-12'}) \
+            .find('div', attrs={'class':'band-profile'})
+
+    name = bandinfo.find('div', attrs={'class':'name'}).getText()
+    description = bandinfo.find('div', attrs={'class':'clan-description'}).getText()
+    badge = url + bandinfo.find('div', attrs={'class':'badge'}) \
+            .find('img', attrs={'class':'band-badge'})['src']
+
+    score = band.find('div', attrs={'class':'row'}) \
+            .find('div', attrs={'class':'col-6'}) \
+            .find('div', attrs={'class':'band-profile-card text-center'}).getText().strip('Trophies')
+    
+    required = band.find('div', attrs={'class':'row'}) \
+            .find_all('div', attrs={'class':'col-6'})[1] \
+            .find('div', attrs={'class':'band-profile-card text-center'}).getText().strip('Required Trophies')
+
+    members = band.find('div', attrs={'class':'container'}) \
+            .find_all('div', attrs={'class':'row'})[2] \
+            .find('div', attrs={'class':'col-12'}) \
+            .find('div', attrs={'class':'members-list'}) \
+            .find('table', attrs={'class':'table brawlstars-table'}) \
+            .find('tbody') \
+            .find_all('tr')
+  
+    _experiences = sorted(members, key=lambda x: int(x.find('td', attrs={'class':'experience'}) \
+                    .find('span', attrs={'class':'experience-star'}).getText()))
+    experiences = []
+    pushers = []
+
+    if len(members) >= 3:
+        for i in range(3):
+            pushername = members[i].find('td', attrs={'class':'player-details'}) \
+                    .find('div', attrs={'class':'player'}) \
+                    .find('div', attrs={'class':'name'}).getText()
+            trophies = members[i].find('td', attrs={'class':'player-details'}) \
+                    .find('div', attrs={'class':'trophy-count'}).getText()
+            pushers.append(
+                f"**{pushername}**"
+                f"\n{trophies} " 
+                f"{emoji(ctx, 'icon_trophy')}\n" 
+                f"#{members[i]['data-href'].strip('/players/')}"
+                )
+
+            xpname = _experiences[i].find('td', attrs={'class':'player-details'}) \
+                    .find('div', attrs={'class':'player'}) \
+                    .find('div', attrs={'class':'name'}).getText()
+            xp = _experiences[i].find('td', attrs={'class':'experience'}) \
+                    .find('span', attrs={'class':'experience-star'}).getText()
+            experiences.append(
+                f"**{xpname}**"
+                f"\n{emoji(ctx, 'star_silver')}"
+                f" {xp}\n" 
+                f"#{_experiences[i]['data-href'].strip('/players/')}"
+                )
+
+    page1 = discord.Embed(description=description, color=random_color())
+    page1.set_author(name=f"{name} (#{tag})")
+    if ctx.bot.psa_message:
+        page1.description = ctx.bot.psa_message
+    page1.set_thumbnail(url=badge)
+    page2 = copy.deepcopy(page1)
+    page2.description = 'Top Players/Experienced Players for this clan.'
+
+    fields1 = [
+        ('Clan Score', f'{score} {emoji(ctx, "icon_trophy")}'),
+        ('Required Trophies', f'{required} {emoji(ctx, "icon_trophy")}'),
+        ('Members', f'{len(members)}/100')
+    ]
+    fields2 = [
+        ("Top Players", '\n\n'.join(pushers)),
+        ("Top Experience", '\n\n'.join(experiences))
+    ]
+
+    for f, v in fields1:
+        page1.add_field(name=f, value=v)
+
+    for f, v in fields2:
+        if v:
+            page2.add_field(name=f, value=v)
+
+    return [page1, page2]
