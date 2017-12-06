b03160dd305b347df9662d2944699a43932d50a5
fourjr <yeejiarong2004@gmail.com>
2017-12-06 20:43:15 +0800

    Syntax error
    editing in a phone is cancer

--- ext/embeds_bs.py
+++ ext/embeds_bs.py
@@ -140,8 +140,8 @@ async def format_band(ctx, soup, tag):
             .find_all('tr')
   
     _experiences = sorted(members, key=lambda x: int(x.find('td', attrs={'class':'experience'} \
-                    .find('span', attrs={'class':'experience-star'}).getText()), reverse)
-    experiences = []
+                    .find('span', attrs={'class':'experience-star'}).getText()), reverse=True) 
+    experiences = [] 
     pushers = []
 
     if len(members) >= 3:
