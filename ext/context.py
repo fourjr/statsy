60ef28fb26d290f0361cfaa821e5da43c061fe5e
verixx <abdurraqeeb53@gmail.com>
2017-12-10 21:08:25 +1100

    Fixed a missing reference

--- ext/context.py
+++ ext/context.py
@@ -122,6 +122,6 @@ class CustomContext(commands.Context):
         elif mode == 'get':
             try:
                 with open(_type + obj + '.json') as f:
-                    return crasync.models.Profile(self.cr, json.load(f))
+                    return crasync.models.Profile(self.bot.cr, json.load(f))
             except (FileNotFoundError, json.decoder.JSONDecodeError):
                 return False
