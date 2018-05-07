77db115ddf71033b064fd847a351021245555805
fourjr <28086837+fourjr@users.noreply.github.com>
2018-05-07 05:40:55 +0000

    lol

--- ext/context.py
+++ ext/context.py
@@ -101,7 +101,7 @@ class CustomContext(commands.Context):
                 pass
         elif mode == 'get':
             try:
-                with open(_type + obj + '.json') as f:
+                with open(_type + tag + '.json') as f:
                     return clashroyale.models.Player(self.bot.cr, json.load(f))
             except (FileNotFoundError, json.decoder.JSONDecodeError):
                 return False
