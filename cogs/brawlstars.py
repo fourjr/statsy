30c4774af9b9ce3944b957a69fae22a0e6bdbe92
fourjr <28086837+fourjr@users.noreply.github.com>
2018-11-02 12:52:20 +0800

    Increase timeout

--- cogs/brawlstars.py
+++ cogs/brawlstars.py
@@ -109,7 +109,7 @@ class Brawl_Stars:
                     async with ctx.session.get(
                         f"http://brawlapi.cf/api{endpoint}",
                         headers={'Authorization': os.getenv('brawlstars')},
-                        timeout=5
+                        timeout=10
                     ) as resp:
                         try:
                             if resp.status == 200:
