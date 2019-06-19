78b4236c0892e6e1b1c24bbb4295d51ac784fceb
fourjr <28086837+fourjr@users.noreply.github.com>
2019-06-20 00:11:27 +0800

    Heroku autostarter support

--- statsbot.py
+++ statsbot.py
@@ -5,6 +5,7 @@ import logging
 import os
 import platform
 import random
+import requests
 import sys
 import traceback
 from collections import defaultdict
@@ -103,6 +104,7 @@ class Statsy(commands.AutoShardedBot):
             print('Fatal exception')
             traceback.print_exc(file=sys.stderr)
         finally:
+            requests.get(f'https://fourjr-herokustartup/logout/{os.getenv("HEROKU_APP_NAME")}')
             if not self.dev_mode:
                 self.backup_task_loop.cancel()
                 self.clan_update.cancel()
@@ -470,6 +472,7 @@ class Statsy(commands.AutoShardedBot):
 
 if __name__ == '__main__':
     load_dotenv(find_dotenv())
+    requests.get(f'https://fourjr-herokustartup/login/{os.getenv("HEROKU_APP_NAME")}')
 
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
